1f98beb60c20e91c1335b1beeb2e69171789eb9c
angel <jjjk>
2023-03-08 01:41:46 +0100

    Falta añadir estado.

--- app/src/main/java/com/example/listadelacompra__miversion/ui/theme/ElementoDeLista.kt
+++ app/src/main/java/com/example/listadelacompra__miversion/ui/theme/ElementoDeLista.kt
@@ -12,6 +12,7 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
 
 @Composable
@@ -46,4 +47,3 @@ fun ElementoDeLista(
             }
         }
     }
-}
